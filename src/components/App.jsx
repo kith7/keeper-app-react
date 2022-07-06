d74ac57c6c2afd5c4937667094c28aac9804e4fc
Kith7 <grzegorz.jaszczak@gmail.com>
2022-07-06 17:12:46 +0200

    Initial keeper app commit

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Heading from "./Heading";
+import Footer from "./Footer";
+import Note from "./Note";
+
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  }
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+  return (
+    <div>
+      <Heading />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const year = new Date().getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright &copy; {year}</p>;
+    </footer>
+  );
+}
+
+export default Footer;
